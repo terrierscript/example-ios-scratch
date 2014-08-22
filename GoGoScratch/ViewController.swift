b02224eb4334b4141a6c724576c17027337a4cce
suisho <suisho@suisho-no-MacBook-Air.local>
2014-08-22 20:15:12 +0900

    init

---
+++ GoGoScratch/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GoGoScratch
+//
+//  Created by suisho on 2014/08/21.
+//  Copyright (c) 2014年 suisho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    @IBOutlet var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
