9214881ab991c32aa132f62fdf39a4528071ff8a
suisho <suisho000000@gmail.com>
2014-08-24 15:52:42 +0900

    add

--- GoGoScratch/GoGoScratch-Bridging-Header.h
+++ GoGoScratch/GoGoScratch-Bridging-Header.h
@@ -1,2 +1,5 @@
 //
-//  Use this file to import your target's public headers that you would like to expose to 
+//  Use this file to import your target's public headers that you would like to expose to
+
+#import "AFNetworking.h"
+#import "EAIntroView.h"
--- Podfile
+++ Podfile
@@ -3,6 +3,7 @@ platform :ios, "7.0"
 
 target "GoGoScratch" do
   pod "AFNetworking", "~>2.0"
+  pod 'EAIntroView', '~> 2.6.0'
 end
 
 target "GoGoScratchTests" do
